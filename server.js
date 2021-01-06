ff3795280d1c3d08573d71e3e1c02c23c34e0159
MAYANKPUROHIT93 <mayank.prohit@gmail.com>
2021-01-07 00:54:15 +0530

    2 commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> {
-  res.send('its working');
+  res.send('its working perfect');
 })
 
 app.post('/signin', (req, res) => {
